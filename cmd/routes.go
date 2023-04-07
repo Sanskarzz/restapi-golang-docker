d57ec112869e563532b8ac6576535443daab674b
Sanskar Gurdasani <92817635+Sanskarzz@users.noreply.github.com>
2023-04-07 13:21:13 +0530

    Create routes.go

---
+++ cmd/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/Sanskarzz/trivia-api/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/", handlers.ListFacts)
+
+	app.Post("/fact", handlers.CreateFact)
+}
